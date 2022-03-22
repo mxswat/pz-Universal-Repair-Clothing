1fd6c0940e7c3133fb59308f91b5608329728688
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-03-22 00:48:52 +0000

    Fixed missing rip menu

--- Contents/mods/RepairAnything/media/lua/client/ISUI/RepairContextMenu.lua
+++ Contents/mods/RepairAnything/media/lua/client/ISUI/RepairContextMenu.lua
@@ -1,5 +1,7 @@
 require "ISUI/ISInventoryPaneContextMenu"
 
+local ripItemsCache = {}
+
 local old_ISInventoryPaneContextMenu_createMenu = ISInventoryPaneContextMenu.createMenu
 ISInventoryPaneContextMenu.createMenu = function(player, isInPlayerInventory, items, x, y, origin)
     local context = old_ISInventoryPaneContextMenu_createMenu(player, isInPlayerInventory, items, x, y, origin)
@@ -22,24 +24,31 @@ ISInventoryPaneContextMenu.createMenu = function(player, isInPlayerInventory, it
 
         if scriptItem and fabricType == nil and clothing:getCoveredParts():size() > 0 then
             scriptItem:DoParam("FabricType = Leather")
+            ripItemsCache[scriptItem:getFullName()] = true
+        end
 
-            if not playerObj:isEquippedClothing(clothing) then
-                local option = context:addOption(getRecipeDisplayName("Rip clothing"), playerObj, ISInventoryPaneContextMenu.onRipClothing, items)
-                -- Item is favourited, add tooltip
-                if clothing:isFavorite() then
-                    local tooltip = ISInventoryPaneContextMenu.addToolTip();
-                    tooltip.description = getText("ContextMenu_CantRipFavourite");
-                    option.toolTip = tooltip;
-                end
-                -- Tool is needed
-                local tools = ClothingRecipesDefinitions["FabricType"][clothing:getFabricType()].tools;
-                if tools and not playerObj:getInventory():getItemFromType(tools, true, true) then
-                    option.notAvailable = true;
-                    local tooltip = ISInventoryPaneContextMenu.addToolTip();
-                    local toolItem = InventoryItemFactory.CreateItem(tools);
-                    tooltip.description = getText("ContextMenu_Require", toolItem:getDisplayName());
-                    option.toolTip = tooltip;
-                end
+        if ripItemsCache[scriptItem:getFullName()] then
+            local option = context:addOption(getRecipeDisplayName("Rip clothing"), playerObj, ISInventoryPaneContextMenu.onRipClothing, items)
+            -- Item is favourited, add tooltip
+            if clothing:isFavorite() then
+                local tooltip = ISInventoryPaneContextMenu.addToolTip();
+                tooltip.description = getText("ContextMenu_CantRipFavourite");
+                option.toolTip = tooltip;
+            end
+            
+            local tools = ClothingRecipesDefinitions["FabricType"][clothing:getFabricType()].tools;
+            if playerObj:isEquippedClothing(clothing) then
+                option.notAvailable = true;
+                local tooltip = ISInventoryPaneContextMenu.addToolTip();
+                tooltip.description = getText("ContextMenu_Require", "Unequip");
+                option.toolTip = tooltip;
+                -- If Tool is needed
+            elseif tools and not playerObj:getInventory():getItemFromType(tools, true, true) then
+                option.notAvailable = true;
+                local tooltip = ISInventoryPaneContextMenu.addToolTip();
+                local toolItem = InventoryItemFactory.CreateItem(tools);
+                tooltip.description = getText("ContextMenu_Require", toolItem:getDisplayName());
+                option.toolTip = tooltip;
             end
         end
     end
